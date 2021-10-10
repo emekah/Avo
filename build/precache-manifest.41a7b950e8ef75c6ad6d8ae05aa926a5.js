b6f617d4f77d94704c8d0889fd36e1ef805ec4bd
Emeka Osuji <peacewillemeka@gmail.com>
2021-10-10 18:21:11 +0100

    build folder

---
+++ build/precache-manifest.41a7b950e8ef75c6ad6d8ae05aa926a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b77a840db2775b8ff5b225687ed4a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3010d25808a086a7ffce",
+    "url": "/static/css/main.89b1cb83.chunk.css"
+  },
+  {
+    "revision": "da454bd2447ef6053870",
+    "url": "/static/js/2.1135e253.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1135e253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3010d25808a086a7ffce",
+    "url": "/static/js/main.8961f3a3.chunk.js"
+  },
+  {
+    "revision": "33d8e6289dca877f5c2b",
+    "url": "/static/js/runtime-main.b24d8771.js"
+  }
+]);
